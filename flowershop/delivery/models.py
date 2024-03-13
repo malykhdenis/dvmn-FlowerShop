5f2ca5933b5c6ff2d4e7a636f1bd11464e1fd9f2
busypenguin <112860511+busypenguin@users.noreply.github.com>
2024-03-13 20:05:41 +0300

    add models

--- flowershop/delivery/models.py
+++ flowershop/delivery/models.py
@@ -1,3 +1,143 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Client(models.Model):
+    '''Клиент'''
+    username = models.CharField(max_length=50,
+                                unique=True,
+                                blank=True,
+                                null=True,
+                                verbose_name='username клиента')
+    address = models.CharField(max_length=200,
+                               blank=True,
+                               null=True,
+                               verbose_name='адрес клиента (куда доставить)')
+    phone_number = models.CharField(max_length=12,
+                                    blank=True,
+                                    null=True,
+                                    verbose_name='номер телефона клиента')
+
+    def __str__(self):
+        return self.username
+
+
+class Reason(models.Model):
+    '''Повод'''
+    name = models.CharField(max_length=200,
+                            blank=True,
+                            null=True,
+                            verbose_name='Название причины')
+
+
+class Flower(models.Model):
+    '''Цветок'''
+    name = models.CharField(max_length=200,
+                            blank=True,
+                            null=True,
+                            verbose_name='Название цветка')
+
+
+class Bouquet(models.Model):
+    '''Букет'''
+    title = models.CharField(max_length=200,
+                             blank=True,
+                             null=True,
+                             verbose_name='Название букета')
+    reasons = models.ManyToManyField(Reason,
+                                     through='BouquetReason',
+                                     blank=True,
+                                     verbose_name='Поводы')
+    flowers = models.ManyToManyField(Flower,
+                                     through="BouquetFlower",
+                                     blank=True,
+                                     verbose_name='Цветы')
+    price = models.IntegerField(blank=True,
+                                null=True,
+                                verbose_name='Цена букета')
+    photo = models.ImageField(blank=True,
+                              null=True,
+                              verbose_name='Фото')
+
+
+class BouquetReason(models.Model):
+    '''Букет по поводу'''
+    bouquet = models.ForeignKey(Reason,
+                                on_delete=models.CASCADE,
+                                blank=True,
+                                null=True)
+    reason = models.ForeignKey(Reason,
+                               on_delete=models.CASCADE,
+                               blank=True,
+                               null=True)
+
+
+class BouquetFlower(models.Model):
+    '''Букет с цветами'''
+    bouquet = models.ForeignKey(Bouquet,
+                                on_delete=models.CASCADE,
+                                blank=True,
+                                null=True,
+                                verbose_name='Название букета')
+    flower = models.ForeignKey(Flower,
+                               on_delete=models.CASCADE,
+                               blank=True,
+                               null=True)
+    amount = models.IntegerField(blank=True,
+                                 null=True,
+                                 verbose_name='Цена букета')
+
+
+class Master(models.Model):
+    '''Мастер'''
+    name = models.CharField(max_length=200,
+                            blank=True,
+                            null=True,
+                            verbose_name='Имя мастера')
+    telegram_id = models.IntegerField(blank=True,
+                                      null=True,
+                                      verbose_name='telegram id мастера')
+
+
+class Courier(models.Model):
+    '''Курьер'''
+    name = models.CharField(max_length=200,
+                            blank=True,
+                            null=True,
+                            verbose_name='Имя курьера')
+    telegram_id = models.IntegerField(blank=True,
+                                      null=True,
+                                      verbose_name='telegram id курьера')
+    orders_count = models.IntegerField(blank=True,
+                                       null=True,
+                                       verbose_name='Цена доставки')
+
+
+class Order(models.Model):
+    '''Заказ'''
+    client_id = models.ForeignKey(Client,
+                                  on_delete=models.CASCADE,
+                                  blank=True,
+                                  null=True)
+    bouquet_id = models.ForeignKey(Bouquet,
+                                   on_delete=models.CASCADE,
+                                   blank=True,
+                                   null=True)
+    date_time = models.DateTimeField(null=True,
+                                     blank=True,
+                                     verbose_name='Время заказа')
+    master_id = models.ForeignKey(Master,
+                                  on_delete=models.CASCADE,
+                                  null=True,
+                                  blank=True)
+    courier_id = models.ForeignKey(Courier,
+                                   on_delete=models.CASCADE,
+                                   null=True,
+                                   blank=True)
+    total_price = models.IntegerField(blank=True,
+                                      null=True,
+                                      verbose_name='Цена букета c доставкой')
+    answer_time = models.TimeField(null=True,
+                                   blank=True,
+                                   verbose_name='Время ответа')
