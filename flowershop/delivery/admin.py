cf38f11fc7b3e5d692b79bc1557d8c5406bf188a
busypenguin <112860511+busypenguin@users.noreply.github.com>
2024-03-14 19:55:01 +0300

    fix M2M

--- flowershop/delivery/admin.py
+++ flowershop/delivery/admin.py
@@ -2,12 +2,26 @@ from django.contrib import admin
 from .models import Client, Bouquet, Courier, Flower, Master, Reason, Order, BouquetReason, BouquetFlower
 # Register your models here.
 
-admin.site.register(Client)
-admin.site.register(Bouquet)
 admin.site.register(Courier)
 admin.site.register(Flower)
 admin.site.register(Master)
 admin.site.register(Reason)
 admin.site.register(Order)
-admin.site.register(BouquetReason)
-admin.site.register(BouquetFlower)
+admin.site.register(Client)
+
+
+class BouquetFlowerInline(admin.TabularInline):
+    model = BouquetFlower
+    extra = 5
+
+
+class BouquetReasonInline(admin.TabularInline):
+    model = BouquetReason
+    extra = 5
+
+
+class BouquetAdmin(admin.ModelAdmin):
+    inlines = (BouquetFlowerInline, BouquetReasonInline)
+
+
+admin.site.register(Bouquet, BouquetAdmin)
