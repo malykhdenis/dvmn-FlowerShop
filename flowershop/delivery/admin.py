88f17958a382349843a404f9755fbd227c7c5133
Denis Malykh <malykhdenis@gmail.com>
2024-03-14 12:07:24 +0300

    register models in admin

--- flowershop/delivery/admin.py
+++ flowershop/delivery/admin.py
@@ -1,3 +1,49 @@
 from django.contrib import admin
 
-# Register your models here.
+from delivery.models import (Bouquet, BouquetFlower, BouquetReason, Client,
+                             Courier, Flower, Master, Order, Reason)
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Reason)
+class ReasonAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Flower)
+class FlowerAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Bouquet)
+class BouquetAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(BouquetReason)
+class BouquetReasonAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(BouquetFlower)
+class BouquetFlowerAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Master)
+class MasterAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Courier)
+class CourierAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    pass
